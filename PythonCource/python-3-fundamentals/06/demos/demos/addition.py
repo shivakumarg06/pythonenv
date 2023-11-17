f950b0e7838b63996a1df8ed24506330691aeead
Shiva Kumar G <skumarg@sompo-intl.com>
2023-11-17 23:12:49 +0530

    charpter 6 function worknote updated

---
+++ PythonCource/python-3-fundamentals/06/demos/demos/addition.py
@@ -0,0 +1,29 @@
+def addition(a, b):
+    return a + b
+
+
+# Main program
+num1 = float(input("Enter your 1st number:\n"))
+num2 = float(input("Enter your 2nd number:\n"))
+
+# Calling our function
+result = addition(num1, num2)
+print("The result is", result)
+
+
+# Example making entire things in fucntions
+def addition(a, b):
+    return a + b
+
+
+def main():
+    # Main program
+    num1 = float(input("Enter your 1st number:\n"))
+    num2 = float(input("Enter your 2nd number:\n"))
+
+    # Calling our function
+    result = addition(num1, num2)
+    print("The result is", result)
+
+
+main()
---
+++ PythonCource/python-3-fundamentals/06/demos/demos/greetings.py
@@ -0,0 +1,10 @@
+def greeting(name):
+    print("Hello", name)
+
+
+# Main Program
+input_name = input("Enter your name:\n")
+greeting(input_name)
+
+input_name2 = input("Enter your name:\n")
+greeting(input_name2)
